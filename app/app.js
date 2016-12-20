ed3c0b142e927d211f5e3f7145f60ff4ef909b17
Jorge Rangel <jer112@txstate.edu>
2016-12-19 20:12:22 -0600

    Fixed styling in app.js Added controller

--- app/app.js
+++ app/app.js
@@ -1,20 +1,37 @@
-'use strict';
+/**Created by Jorge Rangel**/
+/*Handles the main logic of the app. Inject your dependencies in this file*/
 
-// Declare app level module which depends on views, and components
-angular
-    .module('myApp', [
-  'ngMaterial',
-  'ngRoute',
-  'myApp.version'
+(function () {
+  'use strict';
 
-])
-    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  //This will handle the view routing
-  $routeProvider.when('/mainView', {
-    controller: 'View1Ctrl',
-    templateUrl: '/View/mainView.html'
-  });
+  // Declare app level module which depends on views, and components
+  angular.module('myapp', [
+    'ngMaterial',
+    'ngRoute',
+    'myApp.version'
+  ])
+
+    .config(function($locationProvider, $routeProvider) {
+      //This will handle the view routing
+      $routeProvider.when('/mainView', {
+        controller: 'View1Ctrl',
+        templateUrl: 'View/mainView.html'
+      });
+    })
+
+
+  /**
+   * An App controller that changes some of the bounded data in the views if they are not set.
+   */
+  ApplicationController.$inject = ['$scope', '$rootScope', '$state', '$stateParams', '$window'];
+
+  function ApplicationController($scope, $rootScope, $state, $stateParams, $window) {
+
+    var vm = this;
+
+  }
 
 
 
-}]);
+
+})();
