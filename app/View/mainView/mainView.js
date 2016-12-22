88aacac0c3bf19cbf54b98c89a1950c24115d735
Jorge Rangel <jer112@txstate.edu>
2016-12-21 22:43:40 -0600

    Implemented a lot of header items

--- app/View/mainView/mainView.js
+++ app/View/mainView/mainView.js
@@ -19,20 +19,55 @@
 
         //Public functions
         vm.redirectToFacebook = redirectToFacebook;
+        vm.redirectToPhiGam= redirectToPhiGam;
+        vm.redirectToTwitter= redirectToTwitter;
+        vm.redirectToInstagram= redirectToInstagram;
 
         //header image
         vm.headerImg = $sce.trustAsResourceUrl('Images/logo.png');
 
 
+        /*This function, when called, opens fiji in a new window*/
+        function redirectToPhiGam() {
+            try {
+                $window.open('https://www.phigam.org/', '_blank');
+            }
+            catch (error) {
+                console.log("Unable to open phigam.org in a new window " + error);
+            }
+        }
+
         /*This function, when called, opens facebook in a new window*/
         function redirectToFacebook() {
             try {
-                $window.open('https://www.phigam.org/', '_blank');
+                $window.open('https://www.facebook.com/txstfiji/', '_blank');
             }
             catch (error) {
                 console.log("Unable to open Facebook in a new window " + error);
             }
         }
+
+        /*This function, when called, opens twitter in a new window*/
+        function redirectToTwitter() {
+            try {
+                $window.open('https://twitter.com/txstfiji', '_blank');
+            }
+            catch (error) {
+                console.log("Unable to open twitter in a new window " + error);
+            }
+        }
+
+        /*This function, when called, opens instagram in a new window*/
+        function redirectToInstagram() {
+            try {
+                $window.open('https://www.instagram.com/txstfiji/', '_blank');
+            }
+            catch (error) {
+                console.log("Unable to open instagram in a new window " + error);
+            }
+        }
+
+
     }
 
 })();
